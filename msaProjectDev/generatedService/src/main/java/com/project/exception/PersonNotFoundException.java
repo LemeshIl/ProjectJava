dfb6565b669f26fea0c0bba82a2cb781b16175f3
LemeshIl <git12hu32>
2022-04-12 16:32:13 +0400

    fix git ignore2

--- msaProjectDev/generatedService/src/main/java/com/project/exception/PersonNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.project.exception;
-
-import java.util.UUID;
-
-public class PersonNotFoundException extends RuntimeException{
-    public PersonNotFoundException(UUID id){
-        super("Could not find person " + id);
-    }
-}
